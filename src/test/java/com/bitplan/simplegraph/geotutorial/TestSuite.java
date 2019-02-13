778a6246a04f868028672103859825829d221917
Wolfgang Fahl <wf@bitplan.com>
2019-02-13 09:36:33 +0100

    refactors - moves TestChargingStations to com.bitplan.evchargelog project

--- src/test/java/com/bitplan/simplegraph/geotutorial/TestSuite.java
+++ src/test/java/com/bitplan/simplegraph/geotutorial/TestSuite.java
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestRailway.class, TestChargingStations.class })
+@Suite.SuiteClasses({ TestRailway.class })
 /**
  * Test suite
  * 
